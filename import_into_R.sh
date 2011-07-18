b66d80675698ac03f4a00bee9b2028911bbbc0cf
Rob Moss <rob.rgm@gmail.com>
2011-07-18 17:31:32 +0200

    Modified the 'import_into_R' scripts to process +/- delta datasets.

--- import_into_R.sh
+++ import_into_R.sh
@@ -5,23 +5,23 @@ EXT="csv"
 
 TIMES="1m 1h 1d 1w 4w"
 
-if [ "$#" != 1 ]; then
-    echo USAGE: import_into_R.sh results_file
+if [ "$#" != 2 ]; then
+    echo USAGE: import_into_R.sh tag results_file
     exit 2
 fi
 
-if [ ! -r "$1" ]; then
-    echo ERROR: unable to read file \'$1\'
+if [ ! -r "$2" ]; then
+    echo ERROR: unable to read file \'$2\'
     exit 2
 fi
 
 COUNT=1
 
 for T in ${TIMES}; do
-    OUTFILE=${BASE}.${T}.${EXT}
-    echo ./convert_for_R.py -o ${OUTFILE} -t ${COUNT} $1
-    ./convert_for_R.py -o ${OUTFILE} -t ${COUNT} $1
+    OUTFILE=${BASE}.$1.${T}.${EXT}
+    echo ./convert_for_R.py -o ${OUTFILE} -t ${COUNT} $2
+    ./convert_for_R.py -o ${OUTFILE} -t ${COUNT} $2
     COUNT=$(( ${COUNT} + 1 ))
 done
 
-R --no-save < ./import_into_R.R
+# R --no-save < ./import_into_R.R
